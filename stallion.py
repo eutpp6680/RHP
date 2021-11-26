7bf8f939cb9f041bb0275500973d5fb0a2c7e5b6
eutpp6680 <deepimpact6680@gmail.com>
2021-11-26 11:59:11 +0900

    獲得賞金1億円以上の馬の種牡馬のスクレイピング終了

--- stallion.py
+++ stallion.py
@@ -33,29 +33,41 @@ sample_driver.set_page_load_timeout(30)
 
 ad_checker(sample_driver,check=True)
 blood_list = []
+v = True
 
-for x in range(20):
-    x += 2
-    if 4==len(sample_driver.find_elements_by_xpath('//*[@id="contents_liquid"]/div/form/table/tbody/tr['+str(x)+']/td[5]/a')):
-        sample_driver.find_element_by_xpath('//*[@id="contents_liquid"]/div/form/table/tbody/tr['+str(x)+']/td[5]/a[1]').click()
-        sample_driver.set_page_load_timeout(30)
+while v == True:
+    try:
+        for x in range(20):
+            try:
+                x += 2
+                if 4==len(sample_driver.find_elements_by_xpath('//*[@id="contents_liquid"]/div/form/table/tbody/tr['+str(x)+']/td[5]/a')):
+                    sample_driver.find_element_by_xpath('//*[@id="contents_liquid"]/div/form/table/tbody/tr['+str(x)+']/td[5]/a[1]').click()
+                    sample_driver.set_page_load_timeout(30)
 
-        blood_list_1 = []
-        blood_list_1.append(sample_driver.current_url.split("/")[5])
-        ad_checker(sample_driver,check=True)
-        for y in range(32):
-            y += 1
-            for z in range(5):
-                z+=1
-                try:
-                    blood_list_1.append(sample_driver.find_element_by_xpath('//*[@id="contents"]/div[2]/diary_snap/table/tbody/tr[' + str(y) + ']/td['+str(z)+']').text)
-                except:
-                    continue
-        blood_list.append(blood_list_1)
-        sample_driver.back()
-        sample_driver.set_page_load_timeout(30)
-        ad_checker(sample_driver,check=True)
-blood_list = pd.DataFrame(blood_list)
+                    blood_list_1 = []
+                    blood_list_1.append(sample_driver.current_url.split("/")[5])
+                    ad_checker(sample_driver,check=True)
+                    for y in range(32):
+                        y += 1
+                        for z in range(5):
+                            z+=1
+                            try:
+                                blood_list_1.append(sample_driver.find_element_by_xpath('//*[@id="contents"]/div[2]/diary_snap/table/tbody/tr[' + str(y) + ']/td['+str(z)+']').text)
+                            except:
+                                continue
+                    blood_list.append(blood_list_1)
+                    sample_driver.back()
+                    sample_driver.set_page_load_timeout(30)
+                    ad_checker(sample_driver,check=True)
+            except:
+                break
+        blood_list = pd.DataFrame(blood_list)
+        try:
+            sample_driver.find_element_by_xpath('//*[@id="contents_liquid"]/div/div/div[2]/a[2]').click()
+        except:
+            sample_driver.find_element_by_xpath('//*[@id="contents_liquid"]/div/div/div[2]/a').click()
+    except:
+        v = False
 sample_driver.quit()
 
 parent = []
