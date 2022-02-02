aa336a3a03a769ba954832625facea0f5b7598d9
shomas383 <shoma.sawa383@gmail.com>
2022-02-03 00:35:09 +0900

    updated_version

--- bloodline.py
+++ bloodline.py
@@ -24,16 +24,17 @@ driver.get(url)
 sel = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/div[1]/div[1]/div[2]/form/table/tbody/tr[10]/td/select[1]") #NewAge: lower limit
 #sel = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div[1]/div[1]/div[2]/form/table/tbody/tr[10]/td/select[1]") #old_Age: lower limit
 selector = Select(sel)
-selector.select_by_value('3')
+selector.select_by_value('2')
 
 sel2 = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/div[1]/div[1]/div[2]/form/table/tbody/tr[10]/td/select[2]") #NewAge: upper limit
 #sel2 = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div[1]/div[1]/div[2]/form/table/tbody/tr[10]/td/select[2]") #old_Age: upper limit
 selector2 = Select(sel2)
-selector2.select_by_value('3')
+selector2.select_by_value('none')
 
 
 driver.find_element(By.XPATH,"/html/body/div[1]/div[3]/div[1]/div[1]/div[2]/form/table/tbody/tr[9]/td/input[1]").click() #New_Sex
 #driver.find_element(By.XPATH,"/html/body/div[1]/div[2]/div[1]/div[1]/div[2]/form/table/tbody/tr[9]/td/input[1]").click() #old_Sex
+
 
 button = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/div[1]/div[1]/div[2]/form/div/input[1]") #New_Search button
 #button = driver.find_element(By.XPATH, "/html/body/div[1]/div[2]/div[1]/div[1]/div[2]/form/div/input[1]") #old_Search button
@@ -41,6 +42,7 @@ button = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/div[1]/div[1]/d
 actions = ActionChains(driver) 
 actions.move_to_element(button).perform()
 driver.execute_script("arguments[0].scrollIntoView();", button) #
+driver.find_element(By.XPATH,"/html/body/div[1]/div[3]/div[1]/div[1]/div[2]/form/table/tbody/tr[17]/td/input[2]").click() #options
 button.click()
 #driver.set_page_load_timeout(45)
 #time.sleep(5)
@@ -57,8 +59,8 @@ driver.set_page_load_timeout(35)
 #pager = driver.find_element(By.XPATH,"/html/body/div[1]/div[2]/div/div/div[2]")
 
 while True:
-    driver.set_page_load_timeout(35)
-    time.sleep(7)
+    driver.set_page_load_timeout(35) 
+    time.sleep(3)  
     next_button = driver.find_elements(By.XPATH, "/html/body/div[1]/div[3]/div/div/div[2]/a[2]") #Search Next button 
     next_button1st = driver.find_elements(By.XPATH,"/html/body/div[1]/div[3]/div/div/div[2]/a") #Search for 1st Next button
     prev_button = driver.find_elements(By.XPATH,"/html/body/div[1]/div[3]/div/div/div[2]/a[1]") #Search Previous button
@@ -68,20 +70,22 @@ while True:
     
     try:
         for x in range(2,21):
-            time.sleep(3)
+            time.sleep(2)
+            driver.set_page_load_timeout(40)  
             id = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/div/form/table/tbody/tr"+ str([x])+"/td[2]/a").get_attribute('href')
             num_id = re.findall('\d+',id) #Filter only the numbers
             blood = "https://db.netkeiba.com/horse/ped/"+ str(num_id[0])+"/" #Make link for bloodpath   
             data.append([num_id[0],blood]) #Append to a list temporary, create a DataFrame later as a whole
             #----Goes to each horse database
-            WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[3]/div/form/table/tbody/tr"+ str([x])+"/td[2]/a"))).click()
+            WebDriverWait(driver,40).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[3]/div/form/table/tbody/tr"+ str([x])+"/td[2]/a"))).click()
+            #time.sleep(6)
             y = 0 #iterable
             #Loop to get all the table data
-            time.sleep(2)
             while True:  
                 sets = [] 
                 try:   
-                    for z in range(1,50):  
+                    for z in range(1,50):
+                         driver.set_page_load_timeout(40)  
                          date = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/div[4]/div/table/tbody/tr " + str([z]) +"/td[1]/a").get_attribute('innerHTML')
                          held = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/div[4]/div/table/tbody/tr " + str([z]) +"/td[2]/a").get_attribute('innerHTML')
                          weather = driver.find_element(By.XPATH, "/html/body/div[1]/div[3]/div[4]/div/table/tbody/tr " + str([z]) +"/td[3]").get_attribute('innerHTML')
@@ -121,18 +125,19 @@ while True:
             #print(len(next_button))
         print("Completed Batch")
         if(len(next_button1st) == 1):
-            break
-            #WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[3]/div/div/div[2]/a"))).click()
+            time.sleep(5) 
+            WebDriverWait(driver,40).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[3]/div/div/div[2]/a"))).click()
         elif(len(next_button) == 1):
-            WebDriverWait(driver,20).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[3]/div/div/div[2]/a[2]"))).click()
-        else:
+            time.sleep(5)
+            WebDriverWait(driver,40).until(EC.element_to_be_clickable((By.XPATH, "/html/body/div[1]/div[3]/div/div/div[2]/a[2]"))).click()
+        else: 
             break
     except:
         break
 
 
 
-    
+    next_button1st = driver.find_elements(By.XPATH,"/html/body/div[1]/div[3]/div/div/div[2]/a") #Search for 1st Next button
     #next_button = driver.find_elements(By.XPATH, "/html/body/div[1]/div[2]/div/div/div[2]/a[2]")
     #time.sleep(5)
     #next_button.click()
